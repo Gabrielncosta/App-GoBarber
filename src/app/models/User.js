3a64f8d9ce1b0ea3ca61753c06cbb2597319f26b
gabrielncosta <gabrielncosta208@gmail.com>
2019-12-20 16:59:30 -0300

    controllers

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,13 +2,22 @@ import { Model, Sequelize } from 'sequelize';
 
 class User extends Model {
   static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
-      provider: Sequelize.BOOLEAN,
+    super.init( //chamando o método init da classe Model
+      {
+        name: Sequelize.STRING, //colunas que estarão dentro da base de dados
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', () => {
+      
     });
   }
 }
-
+// utilizado para manipular os dados de usuário (criar, deletar, editar...)
 export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController'
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Worldasdasd'})
-})
+
+routes.post('/users', UserController.store);
+
+/* routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name:  'Diego Fernandes',
+    emaiL: 'diego@rocketseat.com.br',
+    password_hash: '1231236',
+  })
+
+  return res.json(user);
+}); */
 
 export default routes;

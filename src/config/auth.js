8e1d6c0d5251f62fff8330b3735405c2259ece15
Gabrielncosta <gabrielncosta208@gmail.com>
2019-12-24 16:02:45 -0300

    bcrypt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '8b812b04cd9115cc9f48fd4d55bb0d2f',
+    expiresIn: '7d'
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import UserController from './app/controllers/UserController'
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 
 routes.post('/users', UserController.store);
-
+routes.post('/sessions', SessionController.store);
 /* routes.get('/', async (req, res) => {
   const user = await User.create({
     name:  'Diego Fernandes',
